73dbf2a871b6e9a5a56445665a780830dd8f52ba
CT <chusakt@gmail.com>
2026-03-10 18:11:15 +0700

    Revert "Set MAX_CONTENT_LENGTH to 50 MB to limit request body size"
    This reverts commit 0800af6e1a162e0b1a43f70bd1970152762ceba2.

--- app.py
+++ app.py
@@ -169,7 +169,6 @@ _summary_thread.start()
 
 # app = Flask(__name__)
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024  # 50 MB
 
 # =====================================
 
